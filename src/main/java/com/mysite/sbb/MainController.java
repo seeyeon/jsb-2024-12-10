f65f7845c8e3cc26c370e749a38e969731690343
seeyeon <gulf72pr@swu.ac.kr>
2024-12-10 11:13:21 +0900

    connect to h2

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,20 +2,24 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public void home(){
+    @ResponseBody
+    public String home(){
          System.out.println("Home page requested!!");
+         return "welcom Spring Home~~";
     }
 
 
-    @GetMapping("/seyeon")
-    public void home2(){
+    @GetMapping("/about")
+    @ResponseBody
+    public String home2(){
         System.out.println("Welcome SeYeon");
-        this.contact();
+        return "안녕하세요";
     }
 
     public void contact(){
